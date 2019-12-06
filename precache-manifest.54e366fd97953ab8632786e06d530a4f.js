856064675febe16eb51777e6754276962450844c
Cornea <100048664@rbc.int>
2019-12-06 09:00:25 +0200

    Updates

---
+++ precache-manifest.54e366fd97953ab8632786e06d530a4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa7b7c64c1d390d65f4b79b61308803",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8f447d8a2e3486950bae",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ee60abaf9809c99271ad",
+    "url": "/robofriends/static/css/main.60e46fa8.chunk.css"
+  },
+  {
+    "revision": "8f447d8a2e3486950bae",
+    "url": "/robofriends/static/js/2.6d59ea6c.chunk.js"
+  },
+  {
+    "revision": "ee60abaf9809c99271ad",
+    "url": "/robofriends/static/js/main.4339f5a3.chunk.js"
+  },
+  {
+    "revision": "cbed4829f530da065628",
+    "url": "/robofriends/static/js/runtime-main.07d9c460.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
